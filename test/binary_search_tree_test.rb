c83dad8c58cc1429c86b2cf0b8c6d68691436bae
Alexis Dumortier <dumortier.alexis@gmail.com>
2019-11-27 15:04:51 -0700

    Add insert and include? methods

---
+++ test/binary_search_tree_test.rb
@@ -0,0 +1,43 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/binary_search_tree'
+require_relative '../lib/node'
+
+class BinarySearchTreeTest < Minitest::Test
+
+  def test_it_exist
+    tree = BinarySearchTree.new
+    assert_instance_of BinarySearchTree, tree
+  end
+
+  def test_insert_returns_tree_depth
+    tree = BinarySearchTree.new
+    assert_equal 0, tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 1, tree.insert(16, "Johnny English")
+    assert_equal 1, tree.insert(92, "Sharknado 3")
+    assert_equal 2, tree.insert(50, "Hannibal Buress: Animal Furnace")
+  end
+
+  def test_include
+    tree = BinarySearchTree.new
+    tree.insert(61, "Bill & Ted's Excellent Adventure")
+    tree.insert(16, "Johnny English")
+    tree.insert(92, "Sharknado 3")
+    tree.insert(50, "Hannibal Buress: Animal Furnace")
+    assert tree.include?(92)
+    refute tree.include?(51)
+  end
+
+  def test_can_return_depth_of_node
+    tree = BinarySearchTree.new
+    tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 0, tree.depth_of(61)
+    tree.insert(16, "Johnny English")
+    assert_equal 1, tree.depth_of(16)
+    tree.insert(92, "Sharknado 3")
+    assert_equal 1, tree.depth_of(92)
+    tree.insert(50, "Hannibal Buress: Animal Furnace")
+    assert_equal 2, tree.depth_of(50)
+  end
+
+end
